aeb7afcbab39001c1c25aa0d9df105e937fefc0a
dh7hong <dh7hong@gmail.com>
2023-10-12 12:48:09 +0900

    combines app.py models.py forms.py and run.py into app.py. Note: models.py should be run first, then manage.py should run, and lastly app.py should run.

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+''' ===================================================
+    app.py
+    
+    We imported Flask, SQLAlchemy to help our
+    Python application communicate with a database, 
+    Bcrypt for passsword hashing, Migrate for database
+    migrations, and several other methods from Flask-Logic 
+    for session management
+    =================================================== '''
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -35,3 +45,253 @@ def create_app():
     
     return app
 
+''' ===================================================
+    end of app.py
+    =================================================== '''
+
+
+
+''' ===================================================
+    models.py
+    =================================================== '''
+
+# from app import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    pwd = db.Column(db.String(300), nullable=False, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+''' ===================================================
+    manage.py
+    =================================================== '''
+
+# def deploy():
+# 	"""Run deployment tasks."""
+# 	from flask_migrate import upgrade,migrate,init,stamp
+
+# 	app = create_app()
+# 	app.app_context().push()
+# 	db.create_all()
+
+# 	# migrate database to latest revision
+# 	init()
+# 	stamp()
+# 	migrate()
+# 	upgrade()
+	
+# deploy()
+
+''' ===================================================
+    forms.py
+    =================================================== '''
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    IntegerField,
+    DateField,
+    TextAreaField,
+)
+
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length, EqualTo, Email, Regexp ,Optional
+import email_validator
+from flask_login import current_user
+from wtforms import ValidationError,validators
+
+
+class login_form(FlaskForm):
+    email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
+    pwd = PasswordField(validators=[InputRequired(), Length(min=8, max=72)])
+    # Placeholder labels to enable form rendering
+    username = StringField(
+        validators=[Optional()]
+    )
+
+
+class register_form(FlaskForm):
+    username = StringField(
+        validators=[
+            InputRequired(),
+            Length(3, 20, message="Please provide a valid name"),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_.]*$",
+                0,
+                "Usernames must have only letters, " "numbers, dots or underscores",
+            ),
+        ]
+    )
+    email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
+    pwd = PasswordField(validators=[InputRequired(), Length(8, 72)])
+    cpwd = PasswordField(
+        validators=[
+            InputRequired(),
+            Length(8, 72),
+            EqualTo("pwd", message="비밀버호는 일치해야 합니다!"),
+        ]
+    )
+
+
+    def validate_email(self, email):
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError("이 메일은 지금 쓰이고있습니다.")
+
+    def validate_uname(self, uname):
+        if User.query.filter_by(username=uname.data).first():
+            raise ValidationError("유저 이름은 벌써쓰이고 있습니다")
+        
+''' ===================================================
+    main.py
+    =================================================== '''
+
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    flash,
+    url_for,
+    session
+)
+
+from datetime import timedelta
+from sqlalchemy.exc import (
+    IntegrityError,
+    DataError,
+    DatabaseError,
+    InterfaceError,
+    InvalidRequestError,
+)
+from werkzeug.routing import BuildError
+
+
+from flask_bcrypt import Bcrypt,generate_password_hash, check_password_hash
+
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
+
+from app import create_app,db,login_manager,bcrypt
+# from models import User
+# from forms import login_form,register_form
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+app = create_app()
+
+@app.before_request
+def session_handler():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=1)
+
+@app.route("/", methods=("GET", "POST"), strict_slashes=False)
+def index():
+    return render_template("index.html",title="Home")
+
+
+@app.route("/login/", methods=("GET", "POST"), strict_slashes=False)
+def login():
+    form = login_form()
+
+    if form.validate_on_submit():
+        try:
+            user = User.query.filter_by(email=form.email.data).first()
+            if check_password_hash(user.pwd, form.pwd.data):
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                flash("틀린 유저이름이나 비밀번호를 입력하셨습니다!", "danger")
+        except Exception as e:
+            flash("틀린 유저이름이나 비밀번호를 입력하셨습니다!", "danger")
+
+    return render_template("auth.html",
+        form=form,
+        text="Login",
+        title="Login",
+        btn_action="Login"
+        )
+
+
+
+# Register route
+@app.route("/register/", methods=("GET", "POST"), strict_slashes=False)
+def register():
+    form = register_form()
+    if form.validate_on_submit():
+        try:
+            email = form.email.data
+            pwd = form.pwd.data
+            username = form.username.data
+            
+            newuser = User(
+                username=username,
+                email=email,
+                pwd=bcrypt.generate_password_hash(pwd),
+            )
+    
+            db.session.add(newuser)
+            db.session.commit()
+            flash(f"계정 만들기가 완료 됐습니다. 축하합니다!", "success")
+            return redirect(url_for("login"))
+
+        except InvalidRequestError:
+
+            db.session.rollback()
+            flash(f"Something went wrong!", "danger")
+
+        except IntegrityError:
+
+            db.session.rollback()
+            flash(f"이 유저는 이미 가입 되었습니다!", "warning")
+
+        except DataError:
+
+            db.session.rollback()
+            flash(f"Invalid Entry", "warning")
+
+        except InterfaceError:
+
+            db.session.rollback()
+            flash(f"Error connecting to the database", "danger")
+
+        except DatabaseError:
+
+            db.session.rollback()
+            flash(f"Error connecting to the database", "danger")
+
+        except BuildError:
+
+            db.session.rollback()
+            flash(f"An error occured !", "danger")
+            
+    return render_template("auth.html",
+        form=form,
+        text="Create account",
+        title="Register",
+        btn_action="Register account"
+        )
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
